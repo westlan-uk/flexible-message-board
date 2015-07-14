ee40e942c7fd62170cf0f28cad62c41e45587831
Chris Franklin <chris@cwfranklin.com>
2015-07-14 16:16:43 +0100

    Added routes to deal with admin login

--- app.js
+++ app.js
@@ -3,13 +3,18 @@ var path = require('path'),
     express = require('express'),
     app = express(),
     server = http.createServer(app),
-    io = require('socket.io').listen(server, { log: true });
+    io = require('socket.io').listen(server, { log: true }),
+    bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + '/public'));
+var jsonParser = bodyParser.json();
+var urlencodedParser = (bodyParser.urlencoded({ extended: false }));
 
 server.listen(process.env.PORT);
 console.log('Server started on port ' + process.env.PORT);
 
+var screen;
+var adminPassword = process.argv.slice(2); // First cli arg
 
 // Routes //
 app.get('/', function(req, res) {
@@ -17,12 +22,38 @@ app.get('/', function(req, res) {
 });
 
 app.get('/control', function(req, res) {
-    res.sendFile(path.join(__dirname, 'control.html'));
+    res.sendFile(path.join(__dirname, 'control/index.html'));
+});
+
+app.get('/control/shoutout', function(req, res) {
+    res.sendFile(path.join(__dirname, 'control/shoutout.html'));
+});
+
+app.get('/control/slides', function(req, res) {
+    res.sendFile(path.join(__dirname, 'control/slides.html'));
+});
+
+app.post('/control/admin', urlencodedParser, function(req, res) {
+    if (req.body.hasOwnProperty('password')) {
+        var pass = req.body.password;
+        
+        console.log("Login attempt: " + pass + " against: " + adminPassword);
+        
+        if (pass == adminPassword) {
+            res.sendFile(path.join(__dirname, 'control/admin.html'));
+        }
+    }
+    else {
+        res.json({ login: "failed" });
+    }
+});
+
+app.get('/status/screen', function (req, res) {
+    var sDef = (screen !== undefined);
+    res.json({ status: sDef });
 });
 // Routes //
 
-
-var screen;
 
 io.sockets.on('connection', function(socket) {
     
@@ -38,8 +69,9 @@ io.sockets.on('connection', function(socket) {
     
     
     // CONTROL //
-    
-    
+    app.post('/shoutout', function(req, res) {
+        console.log('Client submitted shoutout');
+    });
 });
 
 
