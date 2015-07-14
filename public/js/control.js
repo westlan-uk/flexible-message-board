dbecbc4e3095b64d3e60dc48c580b945fce196aa
Chris Franklin <chris@cwfranklin.com>
2015-07-14 17:11:29 +0100

    Fixed relative links for control items and added login / logout JS

--- public/js/control.js
+++ public/js/control.js
@@ -5,14 +5,27 @@ $( document ).ready(function() {
     $.get('/status/screen', function(data) {
         if (data.status === true) {
             screenAvailable = true;
-            var status = $('#screen');
-            var statusImg = $('#screen span');
-            status.removeClass('bg-danger');
-            status.addClass('bg-success');
-            statusImg.removeClass('glyphicon-remove');
-            statusImg.addClass('glyphicon-ok');
+            var screen = $('#screen');
+            var screenImg = $('#screen span');
+            screen.removeClass('bg-danger');
+            screen.addClass('bg-success');
+            screenImg.removeClass('glyphicon-remove');
+            screenImg.addClass('glyphicon-ok');
         }
     });
     
+    $.get('/status/admin', function(data) {
+        if (data.status === true) {
+            adminPermission = true;
+            var admin = $('#admin');
+            var adminImg = $('#admin span');
+            admin.removeClass('bg-danger');
+            admin.addClass('bg-success');
+            adminImg.removeClass('glyphicon-remove');
+            adminImg.addClass('glyphicon-ok');
+            $('#adminLogin').remove();
+            $('#login').append('<form id="adminLogout" action="/control/admin/logout" method="POST"><button id="logoutButton" class="btn btn-default" type="submit">Logout</button></form>');
+        }
+    });
     
 });
