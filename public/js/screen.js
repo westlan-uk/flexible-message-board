cc1cba0532540c37a098cba8dfdacdd1f9f88ac0
Chris Franklin <chris@cwfranklin.com>
2015-09-11 15:26:46 +0100

    Added variables required and basic message display function. Corrected capitalisation

--- public/js/screen.js
+++ public/js/screen.js
@@ -8,7 +8,35 @@ $( document ).ready(function() {
     
     var socket = io.connect();
     
+    var messages = [];
+    var currentMsg = 0;
+    
+    var urgentMessages = [];
+    var currentUrg = 0;
+    
+    var currentMsgStart = 0;
+    
+    function displayMessage(message) {
+        console.log('displaying message: ');
+        console.log(message);
+        resetFrame();
+        
+        currentMsgStart = Math.floor(Date.now() / 1000);
+        
+        switch (message.type) {
+            case 'text':
+                $( "#frame" ).append( message.content );
+                break;
+            case 'shoutout':
+                $( "#frame" ).append( message.content ); // add to template
+                break;
+            default:
+                break;
+        }
+    }
+    
+    // Socket Commands //
     socket.on('connect', function() {
-        console.log('connected');
+        console.log('Connected');
     });
 });
