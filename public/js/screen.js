587c28d4529b008b7269ebb4c939f98953b45954
Chris Franklin <chris@cwfranklin.com>
2015-10-12 10:43:05 +0100

    Cleaned up layout and added additional screen layouts

--- public/js/screen.js
+++ public/js/screen.js
@@ -1,11 +1,62 @@
 function UserInterface() {
 	var self = this;
+	
+	this.headerVisible = true;
+	
+	this.updateFrames = function() {
+		console.log('Updating Frames');
+		var layout = window.state.settings.layout;
+		
+		var frame;
+		
+		if (window.state.isSlideshow) {
+			console.log('Is Slideshow');
+			this.hideHeader();
+			
+			if (layout == "0") {
+				frame = $('<div>').addClass('row').addClass('full-height');
+				frame.append( $('<div>').attr('id', 'slideshow').addClass('col-md-10').addClass('full-height') );
+				frame.append( $('<div>').attr('id', 'ticker').addClass('col-md-2').addClass('full-height') );
+			}
+			else if (layout == "1") {
+				frame = $('<div>').addClass('full-height');
+				frame.append( $('<div>').attr('id', 'slideshow').addClass('layout1-slideshow') );
+				frame.append( $('<div>').attr('id', 'ticker').addClass('layout1-ticker') );
+			}
+			else if (layout == "2") {
+				frame = $('<div>').attr('id', 'slideshow').addClass('layout2-slideshow');
+			}
+		}
+		else {
+			console.log('Is Ticker');
+			this.showHeader();
+			frame = $('<div>').attr('id', 'ticker').addClass('full-height');
+		}
+		
+		this.resetFrame();
+		$( '#frame' ).append(frame);
+		self.addExistingTicks();
+	};
 
-	this.renderMessage = function(message) {
-		var el = $('<p class = "message"></p>');
-		el.html(message.content);
-
-		return el;
+	this.renderSlide = function(message) {
+		this.updateFrames();
+		$('#slideshow').append( $('<div>').append($(message.content)).addClass('auto-margin'));
+	};
+	
+	this.renderTick = function(message) {
+		var tick = $('<p class="message"></p>');
+		tick.html(message.content);
+		
+		$('#ticker').prepend(tick);
+		$('#ticker').find('p').slice(50).remove();
+	};
+	
+	this.addExistingTicks = function() {
+		window.state.messages.forEach(function (message) {
+			if (message.type === 'tick') {
+				self.renderTick(message);
+			}
+		});
 	};
 
 	this.setStatus = function(message, karma) {
@@ -15,13 +66,27 @@ function UserInterface() {
 		$('#status').removeClass('bad good neutral');
 		$('#status').addClass(karma);
 	};
-
-	this.resetFrame = function() {
-		$( "#frame" ).remove();
-		$( "body" ).append( '<div id="frame"></div>' );
+	
+	this.showHeader = function() {
+		if (window.state.ui.headerVisible === false) {
+			window.state.ui.headerVisible = true;
+			$('#header').toggle(2000);
+		}
+	};
+	
+	this.hideHeader = function() {
+		if (window.state.ui.headerVisible === true) {
+			window.state.ui.headerVisible = false;
+			$('#header').toggle(2000);
+		}
 	};
 
-	this.displayMessage = function(message) {
+	this.resetFrame = function() {
+		$( '#frame' ).remove();
+		$( 'body' ).append( '<div id="frame"></div>' );
+	};
+
+	/*this.displayMessage = function(message) {
 		console.log('displaying message: ', message);
 		
 		currentMsgStart = Math.floor(Date.now() / 1000);
@@ -36,7 +101,7 @@ function UserInterface() {
 			default:
 				break;
 		}
-	};
+	};*/
 
 	return this;
 }
