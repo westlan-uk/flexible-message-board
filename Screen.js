8f0441938a4574ffd93154f14c20293dbcb80278
Chris Franklin <chris@cwfranklin.com>
2015-10-12 10:39:04 +0100

    Cleaned up Screen functionality

--- Screen.js
+++ Screen.js
@@ -3,26 +3,49 @@ function Screen(settings) {
 
     this.messages = [];
     
-    this.emitMessagesToEveryone = function() {
-		console.log("Sending messages to all connections, of which there are: " + connections.length);
-
-		connections.forEach(function(connectionHandler) {
-			console.log(connectionHandler.socket.conn.id, "==", connectionHandler.socket.conn.id);
-			self.emitMessagesTo(connectionHandler);
-		});
+    this.emitMessagesToEveryone = function (socketName, data) {
+        console.log("Sending messages to all connections, of which there are: " + connections.length);
+        
+        connections.forEach(function(connectionHandler) {
+            console.log(connectionHandler.socket.conn.id, "==", connectionHandler.socket.conn.id);
+            self.emitMessagesTo(connectionHandler, socketName, data);
+        });
     };
-
-	this.emitMessagesTo = function(connectionHandler) {
-		console.log("emitting messages to conn, count: ", self.messages.length, " id: ", connectionHandler.socket.conn.id);
-		connectionHandler.socket.emit('messages', { messages: self.messages });
-	};
+    
+    this.emitMessagesTo = function(connectionHandler, socketName, data) {
+        console.log("Emitting messages to conn, count: ", self.messages.length, " id: ", connectionHandler.socket.conn.id);
+        connectionHandler.socket.emit(socketName, data);
+    };
     
     this.addMessage = function(message) {
         if (message.added !== undefined && message.added === 0) {
             message.added = Math.floor(Date.now() / 1000);
         }
         
-		self.messages.push(message);
+        if (message.id !== undefined && message.id === 0) {
+            message.id = settings.id++;
+        }
+        
+        self.messages.push(message);
+    };
+    
+    this.updateSettings = function() {
+        this.emitMessagesToEveryone('settings', { settings: settings });
+    };
+    
+    this.sendInitialData = function(connectionHandler) {
+        this.emitMessagesTo(connectionHandler, 'initialise', { messages: self.messages, settings: settings });
+    };
+    
+    this.processMessage = function(message) {
+        if (message.type !== undefined) {
+            message.added = Math.floor(Date.now() / 1000);
+            message.id = settings.id++;
+            
+            self.addMessage(message);
+            
+            self.emitMessagesToEveryone('messages', { message: message });
+        }
     };
     
     this.removeMessage = function(message) {
@@ -33,16 +56,10 @@ function Screen(settings) {
         }
     };
     
-    this.processMessage = function(message) {
-        if (message.type !== undefined) {
-            message.added = Math.floor(Date.now() / 1000);
-            
-            self.addMessage(message);
-            
-            self.emitMessagesToEveryone();
-        }
+    this.sendExpireNotice = function(id) {
+        this.emitMessagesToEveryone('expire', { id: id });
     };
-        
+    
     this.removeExpiredMessages = function() {
         console.log('Checking Expiry @ ' + Math.floor(Date.now() / 1000) + ', message count: ' + self.messages.length);
         var toExpire = [];
@@ -57,11 +74,8 @@ function Screen(settings) {
        
 		toExpire.forEach(function(message) {
             self.removeMessage(message);
-		});
-        
-        if (toExpire.length > 0) {
-            self.emitMessagesToEveryone();
-        }
+            self.sendExpireNotice(message.id);
+        });
     };
 
 	setInterval(this.removeExpiredMessages, settings.expiryCheckInterval * 1000);
