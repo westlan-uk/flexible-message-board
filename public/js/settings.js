3346fabad5fb0446e55593b1883a7e46b0a92a0d
Chris Franklin <chris@cwfranklin.com>
2015-10-16 22:38:08 +0100

    Added settings changes I'd forgotten

--- public/js/settings.js
+++ public/js/settings.js
@@ -15,4 +15,23 @@ $( document ).ready(function() {
             }
         }
     });
+    
+    $.get('/control/admin/slides', function(data) {
+        var table = $('#slideTable');
+        
+        for (var slide in data) {
+            var row = $('<tr>');
+            row.append($( '<td>' ).text(data[slide].id));
+            row.append($( '<td>' ).text(data[slide].added));
+            row.append($( '<td>' ).text(data[slide].content));
+            row.append($( '<td>' ).text(data[slide].expire));
+            row.append($( '<td>' ).text(data[slide].delay));
+            
+            var delCol = $('<td>');
+            delCol.append($( '<a>' ).addClass('inline-delete').text('Delete').attr('href', '/slide/delete?id=' + data[slide].id));
+            row.append(delCol);
+            
+            table.append(row);
+        }
+    });
 });
